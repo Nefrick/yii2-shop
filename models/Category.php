50e191f44817a8d2984763a4c6c4e5832d83c3c0
Nefrick <it.nefrick@gmail.com>
2017-05-04 17:50:01 +0300

    Add Db and view

---
+++ models/Category.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nefrick
+ * Date: 04.05.17
+ * Time: 17:48
+ */
+
+namespace app\models;
+use yii\db\ActiveRecord;
+
+class Category extends ActiveRecord
+{
+
+}
