701b6a8e32dbaee3d9ebd13078430ee4820a5ad9
Nefrick <it.nefrick@gmail.com>
2017-05-17 14:43:52 +0300

    Add CRUD Category

--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -151,6 +151,14 @@ ltAppAsset::register($this);
         </div><!--/header-bottom-->
     </header><!--/header-->
     <div class="container">
+        <?php if(Yii::$app->session->hasFlash('success')): ?>
+            <div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    <?= Yii::$app->session->getFlash('success') ?>
+                </button>
+            </div>
+        <?php endif; ?>
     <?= $content ?>
     </div>
     <footer id="footer"><!--Footer-->
